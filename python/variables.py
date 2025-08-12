44ffb454044129a935b95f06d1ac01f46cda3bb8
HARISHMALISETTY <malisettyharish@gmail.com>
2025-08-12 12:45:22 +0530

    variables

---
+++ python/variables.py
@@ -0,0 +1,50 @@
+# print("hello world")
+
+# x=125
+# y=250
+# x=125
+# y=250
+# x=125
+# y=250
+
+# print(id(x)) #printing x id
+# print(id(y)) #printing y id
+
+# x=4
+# y=5
+# z=10
+
+# x,y,z=4,5,10 
+
+#input--->used to take the values from the user
+
+
+# a=int(input("enter the a value : "))
+# b=int(input("enter the b value : "))
+
+# # print(a,"is the entered value")
+# print(f"{a} is the entered value")
+# print(f"{b} is the entered value")
+# print(f"{a+b} is the sum of them")
+
+# a="25"
+# # int()-->converts string to num
+# b=int(a)
+# print(type(b))
+
+# a="harish"
+# b=float(a)
+
+# print(type(b),b)
+
+
+# "125"--->int()
+# "floar"--->float()
+# num to string-->str()
+
+# a=125
+# b=str(a)
+# print(type(b))
+
+
+
